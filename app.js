b95cc7207fce4288b3c73488f20642388ea9fa20
frenchiesvseverybody <frenchiesvseverybody@gmail.com>
2025-11-05 17:05:51 -0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-import {gestureDetectorComponent, playVideoComponent} from './components.js'
-
-console.log(5)
-AFRAME.registerComponent('play-video-component', playVideoComponent)
-AFRAME.registerComponent('gesture-detector', gestureDetectorComponent)
-
-console.log(22)
